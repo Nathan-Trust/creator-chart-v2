91425108c62ff8719f7a3762a224711d365bea21
Nathan-Trust <nathantrustekanem@gmail.com>
2026-01-22 02:10:03 +0100

    feat:updated code

--- app/pricing/checkout/page.tsx
+++ app/pricing/checkout/page.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import CheckoutClient from "./client";
 
 export const metadata = {
@@ -7,5 +8,9 @@ export const metadata = {
 };
 
 export default function CheckoutPage() {
-  return <CheckoutClient />;
+    return (
+      <Suspense>
+        <CheckoutClient />
+      </Suspense>
+    );
 }
